3a2b1bffda38fab8b9547a24835d1907a72fa062
manarnouh00 <manarnouh4@gmail.com>
2022-05-22 01:28:21 +0200

    update

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,42 @@
-const functions = require("firebase-functions");
+"use strict";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.calculateFees = functions.firestore
+    .document("/users/{userId}/history/{hisID}")
+    .onUpdate((change, context) => {
+      const entryTime = change.after.get("EntryTimestamp");
+      const exitTime = change.after.get("ExitTimestamp");
+      if (exitTime == entryTime) {
+        return change.after.ref.set(
+            {
+              Fees: 0,
+            },
+            {
+              merge: true,
+            }
+        );
+      }
+
+      let fees = 0;
+      let isLessThan1Hour = true;
+      const timeDiff = exitTime._seconds - entryTime._seconds;
+
+      isLessThan1Hour = Math.abs(timeDiff) < 3600;
+      if (isLessThan1Hour) {
+        fees = 5;
+      } else {
+        fees = 5 + (timeDiff / 3600) * 1.5;
+      }
+
+      return change.after.ref.set(
+          {
+            Fees: fees,
+          },
+          {
+            merge: true,
+          }
+      );
+    });
